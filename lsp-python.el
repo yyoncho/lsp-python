78153077b0c15db662ff292ff5a352abe2f1d2a8
Vibhav Pant <vibhavp@gmail.com>
2017-04-18 15:20:45 +0530

    Initial Commit

---
+++ lsp-python.el
@@ -0,0 +1,19 @@
+;;; lsp-python.el --- Python support for lsp-mode
+
+;; Copyright (C) 2017 Vibhav Pant <vibhavp@gmail.com>
+
+;; Author: Vibhav Pant <vibhavp@gmail.com>
+;; Version: 1.0
+;; Package-Requires: ((lsp-mode "2.0"))
+;; Keywords: python
+;; URL: https://github.com/emacs-lsp/lsp-python
+
+(require 'lsp-mode)
+(require 'python)
+
+;;;###autoload
+(lsp-define-client 'python-mode "python" 'stdio #'(lambda () default-directory)
+  :command '("pyls")
+  :name "Python Language Server")
+
+(provide 'lsp-python)
